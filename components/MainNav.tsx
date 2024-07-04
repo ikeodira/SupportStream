86b2e0cd772b1d7da67276332516461a58698c01
Ike Odira <odiraike8@gmail.com>
2024-07-04 15:26:52 +0100

    structure complete

---
+++ components/MainNav.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+function MainNav() {
+    return (
+        <div className='flex justify-between'>
+            <div className='flex items-center gap-2'>
+                <Link href="/">Dashboard</Link>
+                <Link href="/tickets">Tickets</Link>
+                <Link href="/users">Users</Link>
+            </div>
+            <div className='flex items-center gap-2'>
+                <Link href="/users">Logout</Link>
+                <Link href="/users">Dark</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default MainNav
